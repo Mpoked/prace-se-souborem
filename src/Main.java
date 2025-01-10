cb182f264590c00aabc93b7141c4e07025471930
Mpoked <167511502+Mpoked@users.noreply.github.com>
2025-01-10 13:11:18 +0100

    nahární souborů

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main {
                             String krestniJmeno = casti[1]; // Křestní jméno
 
                             // Kontrola podmínek
-                            if ("Ivana".equals(krestniJmeno) && idZakaznika < 500) {
+                            if ("Ivana".equals(krestniJmeno) && idZakaznika < 50) {
                                 System.out.println(idZakaznika + " " +krestniJmeno);
                             }
                         }
